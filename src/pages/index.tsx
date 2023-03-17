65bc1f86a70425c3c083818d1a6a30f74ace9d47
Christian Castaneda <christianjc_09@yahoo.com>
2023-03-17 02:58:57 -0700

    added name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,19 +38,22 @@ export default function Home() {
                         </a>
                     </div>
                 </div>
+                <div>
+                    <h1>Berkeley Student Cooperative App</h1>
+                </div>
 
                 <div className={styles.center}>
                     <div>
-                        <h1>Berkeley Student Cooperative App</h1>
+                        <Image
+                            className={styles.logo}
+                            src='/next.svg'
+                            alt='Next.js Logo'
+                            width={180}
+                            height={37}
+                            priority
+                        />
                     </div>
-                    <Image
-                        className={styles.logo}
-                        src='/next.svg'
-                        alt='Next.js Logo'
-                        width={180}
-                        height={37}
-                        priority
-                    />
+
                     <div className={styles.thirteen}>
                         <Image src='/thirteen.svg' alt='13' width={40} height={31} priority />
                     </div>
